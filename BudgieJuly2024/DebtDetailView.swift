32c392ebd43b0e37421469dfdb4dc804832bf791
Tony Rocca <trocca36@gmail.com>
2024-07-25 23:23:57 -0500

    updated the debt allocation view

--- BudgieJuly2024/DebtDetailView.swift
+++ BudgieJuly2024/DebtDetailView.swift
@@ -7,64 +7,106 @@ struct DebtDetailView: View {
     var hasSavingsGoals: Bool
     @EnvironmentObject var budgetCategoryStore: BudgetCategoryStore
 
+    @State private var debtAmounts: [UUID: String] = [:]
+    @State private var selectedDates: [UUID: Date] = [:]
+    @State private var showDatePicker: UUID? = nil
+
+    private let currentYear = Calendar.current.component(.year, from: Date())
+    private var years: [Int]
+
+    init(income: Binding<String>, paymentFrequency: Binding<PaymentCadence>, hasExpenses: Bool, hasSavingsGoals: Bool) {
+        self._income = income
+        self._paymentFrequency = paymentFrequency
+        self.hasExpenses = hasExpenses
+        self.hasSavingsGoals = hasSavingsGoals
+        self.years = Array(currentYear...currentYear + 10)
+    }
+
+    private let currencyFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = Locale.current
+        formatter.maximumFractionDigits = 2
+        formatter.minimumFractionDigits = 2
+        return formatter
+    }()
+
     var body: some View {
-        VStack {
-            VStack(spacing: 10) {
-                Text("Enter your debt details")
-                    .font(.title2)
-                    .fontWeight(.bold)
-                    .padding(.top, 20)
-                
-                Text("Please enter the amount you owe and the due date for each type of debt.")
-                    .font(.body)
-                    .foregroundColor(.gray)
-                    .padding(.horizontal, 20)
-                    .multilineTextAlignment(.center)
-            }
-            .padding(.bottom, 20)
+        VStack(spacing: 16) {
+            Text("Enter your debt details")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.top, 16)
 
-            List {
-                ForEach(budgetCategoryStore.categories.filter { $0.type == .debt && $0.isSelected }) { category in
-                    VStack(alignment: .leading, spacing: 10) {
-                        HStack {
-                            Text("\(category.emoji) \(category.name)")
-                                .font(.headline)
-                            Spacer()
-                        }
+            Text("Enter your debt amount and when it is due.")
+                .font(.subheadline)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 16)
 
-                        TextField("Amount", value: Binding(
-                            get: { category.amount ?? 0 },
-                            set: { newValue in
-                                if let index = budgetCategoryStore.categories.firstIndex(where: { $0.id == category.id }) {
-                                    budgetCategoryStore.categories[index].amount = newValue
+            ScrollView {
+                VStack(alignment: .leading, spacing: 12) {
+                    ForEach(budgetCategoryStore.categories.filter { $0.type == .debt && $0.isSelected }) { category in
+                        VStack(alignment: .leading, spacing: 8) {
+                            HStack {
+                                Text("\(category.emoji) \(category.name)")
+                                    .font(.headline)
+                                Spacer()
+                            }
+
+                            HStack {
+                                Text("$")
+                                    .padding(.leading, 16)
+                                TextField("Enter your debt amount", text: Binding(
+                                    get: { debtAmounts[category.id] ?? "" },
+                                    set: { debtAmounts[category.id] = $0 }
+                                ))
+                                .textFieldStyle(PlainTextFieldStyle())
+                                .padding(12)
+                                .background(Color(UIColor.systemGray5))
+                                .cornerRadius(8)
+                                .keyboardType(.decimalPad)
+                                .onChange(of: debtAmounts[category.id] ?? "") { newValue in
+                                    debtAmounts[category.id] = formatCurrencyInput(newValue)
                                 }
-                            }), formatter: NumberFormatter())
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                        
-                        DatePicker("Due Date", selection: Binding(
-                            get: { category.dueDate ?? Date() },
-                            set: { newValue in
-                                if let index = budgetCategoryStore.categories.firstIndex(where: { $0.id == category.id }) {
-                                    budgetCategoryStore.categories[index].dueDate = newValue
-                                }
-                            }),
-                            in: Date()..., // Only allow future dates
-                            displayedComponents: .date
-                        )
-                        .datePickerStyle(WheelDatePickerStyle())
-                        .labelsHidden()
-                        .onAppear {
-                            if category.dueDate == nil {
-                                if let index = budgetCategoryStore.categories.firstIndex(where: { $0.id == category.id }) {
-                                    budgetCategoryStore.categories[index].dueDate = Date()
+                                .multilineTextAlignment(.trailing)
+                                .padding(.trailing, 16)
+                                
+                                Button(action: {
+                                    if showDatePicker == category.id {
+                                        showDatePicker = nil
+                                    } else {
+                                        showDatePicker = category.id
+                                    }
+                                }) {
+                                    Image(systemName: "calendar")
+                                        .padding(.trailing, 16)
                                 }
                             }
+                            .background(Color(UIColor.systemGray5))
+                            .cornerRadius(8)
+                            .padding(.horizontal, 16)
+
+                            if showDatePicker == category.id {
+                                DatePicker(
+                                    "Select Due Date",
+                                    selection: Binding(
+                                        get: { selectedDates[category.id] ?? Date() },
+                                        set: { selectedDates[category.id] = $0 }
+                                    ),
+                                    displayedComponents: [.date]
+                                )
+                                .datePickerStyle(GraphicalDatePickerStyle())
+                                .labelsHidden()
+                                .padding(.horizontal, 16)
+                            }
                         }
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(12)
+                        .shadow(color: Color.black.opacity(0.1), radius: 10, x: 0, y: 5)
                     }
-                    .padding(.vertical, 10)
                 }
             }
-            .listStyle(InsetGroupedListStyle())
 
             Spacer()
 
@@ -85,21 +127,27 @@ struct DebtDetailView: View {
             }
             .padding(.bottom, 50)
         }
-        .navigationTitle("Enter Debt Details")
         .background(Color(UIColor.systemGroupedBackground).edgesIgnoringSafeArea(.all))
     }
 
-    @ViewBuilder
+    private func formatCurrencyInput(_ input: String) -> String {
+        let filtered = input.filter { "0123456789".contains($0) }
+        if let value = Double(filtered) {
+            return String(format: "%.2f", value / 100)
+        }
+        return ""
+    }
+
     private func nextView() -> some View {
         if hasExpenses {
-            ExpenseSelectionView(income: $income, paymentFrequency: $paymentFrequency, hasSavingsGoals: hasSavingsGoals)
-                .environmentObject(budgetCategoryStore)
+            return AnyView(ExpenseSelectionView(income: $income, paymentFrequency: $paymentFrequency, hasSavingsGoals: hasSavingsGoals)
+                .environmentObject(budgetCategoryStore))
         } else if hasSavingsGoals {
-            SavingsSelectionView(income: $income, paymentFrequency: $paymentFrequency)
-                .environmentObject(budgetCategoryStore)
+            return AnyView(SavingsSelectionView(income: $income, paymentFrequency: $paymentFrequency)
+                .environmentObject(budgetCategoryStore))
         } else {
-            ContentView(selectedCategories: budgetCategoryStore.categories.filter { $0.isSelected }, paymentFrequency: paymentFrequency, paycheckAmountText: income)
-                .environmentObject(budgetCategoryStore)
+            return AnyView(ContentView(selectedCategories: budgetCategoryStore.categories.filter { $0.isSelected }, paymentFrequency: paymentFrequency, paycheckAmountText: income)
+                .environmentObject(budgetCategoryStore))
         }
     }
 }
