50f4d5d0b739cd1e5093289e8cbdd6057a0c8101
Tony Rocca <trocca36@gmail.com>
2024-08-16 16:23:59 -0400

    updated the add category functionality for expenses

--- BudgieJuly2024/DebtSelectionView.swift
+++ BudgieJuly2024/DebtSelectionView.swift
@@ -11,95 +11,162 @@ struct DebtSelectionView: View {
     var hasExpenses: Bool
     var hasSavingsGoals: Bool
 
+    private let lightGrayColor = Color(UIColor.systemGray6)
+
     var body: some View {
-        VStack(spacing: 16) {
-            // Header
-            VStack(alignment: .leading, spacing: 4) {
-                Text("Select your debt categories")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .padding(.top, 16)
-                    .foregroundColor(.primary)
+        ZStack {
+            VStack(spacing: 16) {
+                // Header
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Select your debt categories")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.primary)
 
-                Text("Choose the debt you currently are paying off.")
-                    .font(.headline)
-                    .fontWeight(.regular)
-                    .foregroundColor(.secondary)
-            }
-            .padding(.horizontal, 16)
-            .padding(.bottom, 8)
+                    Text("Choose the debt you currently are paying off.")
+                        .font(.headline)
+                        .fontWeight(.regular)
+                        .foregroundColor(.secondary)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.top, 16)
+                .padding(.bottom, 8)
+                .padding(.horizontal, 16)
 
-            // Categories
-            VStack(spacing: 0) {
-                ForEach(budgetCategoryStore.categories.filter { $0.type == .debt }) { category in
-                    ToggleRow(isOn: Binding(
-                        get: { selectedDebtCategories.contains(category.id) },
-                        set: { newValue in
-                            if newValue {
-                                selectedDebtCategories.append(category.id)
-                                if let index = budgetCategoryStore.categories.firstIndex(where: { $0.id == category.id }) {
-                                    budgetCategoryStore.categories[index].isSelected = true
-                                }
-                            } else {
-                                if let index = selectedDebtCategories.firstIndex(of: category.id) {
-                                    selectedDebtCategories.remove(at: index)
-                                    if let catIndex = budgetCategoryStore.categories.firstIndex(where: { $0.id == category.id }) {
-                                        budgetCategoryStore.categories[catIndex].isSelected = false
+                // Categories
+                ScrollView {
+                    VStack(spacing: 16) {
+                        ForEach(budgetCategoryStore.categories.filter { $0.type == .debt }) { category in
+                            VStack(spacing: 0) {
+                                ToggleRow(isOn: Binding(
+                                    get: { selectedDebtCategories.contains(category.id) },
+                                    set: { newValue in
+                                        if newValue {
+                                            selectedDebtCategories.append(category.id)
+                                            if let index = budgetCategoryStore.categories.firstIndex(where: { $0.id == category.id }) {
+                                                budgetCategoryStore.categories[index].isSelected = true
+                                            }
+                                        } else {
+                                            if let index = selectedDebtCategories.firstIndex(of: category.id) {
+                                                selectedDebtCategories.remove(at: index)
+                                                if let catIndex = budgetCategoryStore.categories.firstIndex(where: { $0.id == category.id }) {
+                                                    budgetCategoryStore.categories[catIndex].isSelected = false
+                                                }
+                                            }
+                                        }
                                     }
+                                ), icon: category.emoji, text: category.name)
+                                
+                                if category.id != budgetCategoryStore.categories.filter({ $0.type == .debt }).last?.id {
+                                    Divider()
                                 }
                             }
                         }
-                    ), icon: category.emoji, text: category.name)
-                    if category.id != budgetCategoryStore.categories.filter({ $0.type == .debt }).last?.id {
-                        Divider()
+                        
+                        Button(action: {
+                            showAddDebtForm = true
+                        }) {
+                            HStack {
+                                Image(systemName: "plus.circle.fill")
+                                    .foregroundColor(.blue)
+                                Text("Add Category")
+                                    .foregroundColor(.blue)
+                                    .font(.subheadline)
+                                    .fontWeight(.medium)
+                            }
+                            .frame(maxWidth: .infinity)
+                            .padding(.vertical, 10)
+                            .background(lightGrayColor)
+                            .cornerRadius(8)
+                        }
+                        .padding(.horizontal, 16)
+                        .padding(.vertical, 12)
                     }
-                }
-                Button(action: {
-                    withAnimation {
-                        showAddDebtForm = true
-                    }
-                }) {
-                    HStack {
-                        Text("🔧")
-                        Text("Add Category")
-                            .foregroundColor(.primary)
-                        Spacer()
-                        Text("Add")
-                            .foregroundColor(.blue)
-                    }
-                    .padding(.vertical, 8)
+                    .background(Color.white)
+                    .cornerRadius(10)
                     .padding(.horizontal, 16)
                 }
-            }
-            .background(Color.white)
-            .cornerRadius(10)
-            .padding(.horizontal, 16)
 
-            Spacer()
+                Spacer()
 
-            NavigationLink(destination: DebtDetailView(income: $income, paymentFrequency: $paymentFrequency, hasExpenses: hasExpenses, hasSavingsGoals: hasSavingsGoals)
-                .environmentObject(budgetCategoryStore)) {
-                Text("Next")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(
-                        LinearGradient(gradient: Gradient(colors: [Color.green, Color.blue]),
-                                       startPoint: .topLeading,
-                                       endPoint: .bottomTrailing)
-                    )
-                    .cornerRadius(10)
-                    .shadow(radius: 5)
+                NavigationLink(destination: DebtDetailView(income: $income, paymentFrequency: $paymentFrequency, hasExpenses: hasExpenses, hasSavingsGoals: hasSavingsGoals)
+                    .environmentObject(budgetCategoryStore)) {
+                    Text("Next")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(
+                            LinearGradient(gradient: Gradient(colors: [Color.green, Color.blue]),
+                                           startPoint: .topLeading,
+                                           endPoint: .bottomTrailing)
+                        )
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+                }
+                .padding(.horizontal, 16)
+                .padding(.bottom, 50)
+                .disabled(selectedDebtCategories.isEmpty)
             }
-            .padding(.horizontal, 16)
-            .padding(.bottom, 50)
-            .disabled(selectedDebtCategories.isEmpty)
+            .background(Color(UIColor.systemGroupedBackground).edgesIgnoringSafeArea(.all))
+
+            if showAddDebtForm {
+                Color.black.opacity(0.4)
+                    .edgesIgnoringSafeArea(.all)
+                    .onTapGesture {
+                        showAddDebtForm = false
+                    }
+
+                VStack(spacing: 20) {
+                    Text("Add New Debt Category")
+                        .font(.headline)
+                        .padding(.top, 20)
+                        .foregroundColor(.primary)
+
+                    TextField("Category Name", text: $newDebtName)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.horizontal)
+
+                    Button(action: addDebtCategory) {
+                        Text("Add")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(
+                                LinearGradient(gradient: Gradient(colors: [Color.blue, Color.blue.opacity(0.8)]),
+                                               startPoint: .topLeading,
+                                               endPoint: .bottomTrailing)
+                            )
+                            .cornerRadius(10)
+                    }
+                    .padding(.horizontal)
+                    .opacity(newDebtName.isEmpty ? 0.6 : 1.0)
+                    .disabled(newDebtName.isEmpty)
+                }
+                .frame(width: 300, height: 200)
+                .background(Color(UIColor.systemBackground))
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .transition(.move(edge: .bottom))
+            }
         }
-        .background(Color(UIColor.systemGroupedBackground).edgesIgnoringSafeArea(.all))
-        .sheet(isPresented: $showAddDebtForm) {
-            AddDebtCategoryView(isPresented: $showAddDebtForm, newDebtName: $newDebtName, budgetCategoryStore: budgetCategoryStore, selectedDebtCategories: $selectedDebtCategories)
-        }
+    }
+
+    private func addDebtCategory() {
+        let newCategory = BudgetCategory(
+            name: newDebtName,
+            emoji: "💳",
+            allocationPercentage: 0.0,
+            subcategories: [],
+            description: "Custom debt",
+            type: .debt,
+            isSelected: true
+        )
+        budgetCategoryStore.addCategory(newCategory)
+        selectedDebtCategories.append(newCategory.id)
+        showAddDebtForm = false
+        newDebtName = ""
     }
 }
 
@@ -118,61 +185,6 @@ struct ToggleRow: View {
         .toggleStyle(SwitchToggleStyle(tint: Color.blue))
         .padding(.vertical, 8)
         .padding(.horizontal, 16)
-    }
-}
-
-struct AddDebtCategoryView: View {
-    @Binding var isPresented: Bool
-    @Binding var newDebtName: String
-    var budgetCategoryStore: BudgetCategoryStore
-    @Binding var selectedDebtCategories: [UUID]
-
-    var body: some View {
-        VStack(spacing: 16) {
-            Text("Add New Debt Category")
-                .font(.headline)
-                .padding(.top, 16)
-                .foregroundColor(.primary)
-
-            TextField("Name", text: $newDebtName)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-                .background(Color(UIColor.systemGray6))
-                .cornerRadius(10)
-                .padding(.horizontal)
-
-            Button(action: {
-                withAnimation {
-                    isPresented = false
-                }
-                let newCategory = BudgetCategory(
-                    name: newDebtName,
-                    emoji: "💳",
-                    allocationPercentage: 0.0,
-                    subcategories: [],
-                    description: "Custom debt",
-                    type: .debt,
-                    isSelected: true
-                )
-                budgetCategoryStore.addCategory(newCategory)
-                selectedDebtCategories.append(newCategory.id)
-            }) {
-                Text("Add")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .cornerRadius(10)
-                    .padding(.horizontal)
-            }
-            .padding(.bottom, 16)
-        }
-        .frame(width: 300, height: 200)
-        .background(Color(UIColor.systemBackground))
-        .cornerRadius(20)
-        .shadow(radius: 20)
-        .transition(.move(edge: .bottom))
     }
 }
 
